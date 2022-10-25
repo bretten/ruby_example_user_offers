5597a33739eb019d8080714f569065e3a64f658d
Brett <bretten@users.noreply.github.com>
2022-10-25 03:26:42 -0700

    #24 Added database seed file and instructions in the README (#35)

--- user_offers/db/seeds.rb
+++ user_offers/db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+User.create(email: "user1@example.com", first_name: "Jane", last_name: "Doe", birthdate: "1980-01-01", gender: 0, password: "password!", password_confirmation: "password!")
+User.create(email: "user2@example.com", first_name: "John", last_name: "Doe", birthdate: "1999-01-01", gender: 1, password: "password!", password_confirmation: "password!")
+
+100.times { |i|
+  offer = Offer.create({ description: Faker::Marketing.buzzwords })
+  gender = 0
+  if i >= 25 and i < 50
+    gender = 1
+  elsif i >= 50 and i < 75
+    gender = 2
+  elsif i >= 75 and i <= 100
+    gender = 3
+  end
+
+  10.times { |j|
+    OfferDemographic.create(minimum_age: j * 10, maximum_age: (j + 1) * 10, gender: gender, offer_id: offer.id)
+  }
+}
